d9be60e2517aa0f2a52a860fe2142db47584e38f
barmoshe <1barmoshe1@gmail.com>
2021-11-07 12:48:19 +0200

    added promt to each Q

--- exe.c
+++ exe.c
@@ -12,17 +12,17 @@
 
 void q1()
 {
-	
+	printf("you chose Q1, get ready to be amazed\n");	
 }
 
 void q2()
 {
-
+	printf("you chose Q2, your'e about to be sent to outter space\n");	
 }
 
 void q3()
 {
-
+	printf("houston we have a problem he chose the user chose Q3\n");	
 }
 
 
