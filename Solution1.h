8d0c024267a7b7104f129976baa6b2c8a79e5244
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2021-11-08 09:08:19 +0200

    hellobarmyfriend

--- Solution1.h
+++ Solution1.h
@@ -1,6 +1,6 @@
 #ifndef SOLUTION1_H_
 #define SOLUTION1_H_
-int doFindMaxSubMatrix(int* mat,int BMrow,int BMcol,int SMrow,int SMcol);
+int Q1_doFindMaxSubMatrix(int* mat,int BMrow,int BMcol,int SMrow,int SMcol);
 
 
 #endif 
