0d9f929ff14d5498715492fc9acf48aaed3985fa
barmoshe <1barmoshe1@gmail.com>
2021-11-08 08:09:55 +0200

    Update exe.c

--- exe.c
+++ exe.c
@@ -13,8 +13,9 @@
 void q1()
 {
 	printf("you chose Q1, get ready to be amazed\n");
-	int a[3][3] ={{1,-1,36},{1,1,50},{4,50,49}};
-	int sum=sumSmallMat((int*)a, 2, 2,3);
+	int a[3][3] ={{1,-1,36,3},{1,1,50,3},{4,50,49,3}};
+	int sum=sumSmallMat((int*)a, 2, 2,4);
+	printf("%d",sum);
 	/*int rowBM,colBM,rowSM,colSMl;
 	printf("you need to tell me how many rows and cols you would like:  \n");
 	printf("ROWS:	");
