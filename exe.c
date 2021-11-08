86e5b081d75918fe8a61daf6c8875a6178c81a9f
barmoshe <1barmoshe1@gmail.com>
2021-11-08 10:14:56 +0200

    Update exe.c

--- exe.c
+++ exe.c
@@ -5,22 +5,46 @@
 #include "Utill.h"
 #include "exe.h"
 
-#define SIZE 6
 #define ROWS 5
-#define COLS 3
+#define COLS 5
 
 void q1Menu(int* rowBM,int* colBM,int* rowSM,int* colSM)
 {
+	int a=0;
+	while(a=0)
+	{
 printf("you need to tell me how many rows and cols you would like:  \n");
 	printf("ROWS:	");
 	scanf("%d",rowBM);
+	if(rowBM>ROWS)
+	{
+		a++;
+		printf("small rows to big")
+	}
 	printf("COLS:	");
 	scanf("%d",colBM);
+	if(colBM>COLS)
+	{
+		a++;
+		printf("small rows to big")
+	}
 	printf("Now tell me the portion of the small mat you would like:  \n");
 	printf("ROWS:	");
 	scanf("%d",rowSM);
+	if(rowSM>rowBM)
+	{
+		a++;
+		printf("small rows to big")
+	}
+
 	printf("COLS:	");
 	scanf("%d",colSM);
+		if(colSM>colBM)
+	{
+		a++;
+		printf("small cols to big")
+	}
+	}
 }
 void q1()
 {
@@ -29,11 +53,11 @@ void q1()
 	printf("you chose Q1, get ready to be amazed\n");
 	int rowBM,colBM,rowSM,colSM;
 	q1Menu(&rowBM,&colBM,&rowSM,&colSM);
-	int mat[rowBM][colBM];
+	int mat[ROWS][COLS];
 	initMatRand((int*) mat,  rowBM,  colBM);
 	printf("the big mat:    \n");
 	printMat((int*) mat,  rowBM,  colBM);
-	sum=Q1_doFindMaxSubMatrix((int*) mat, rowBM, colBM, rowSM, colSM,&answerROW,&answerCOL);
+	sum=Q1_doFindMaxSubMatrix((int*) mat, rowBM, colBM, rowSM, colSM, &answerROW ,&answerCOL);
 	printf("\n the sum is :   %d  \n",sum);	
 		
 }
