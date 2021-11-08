a0803ccb653aee637adc0b057e6d5922c68e675a
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2021-11-08 08:56:51 +0200

    gabi-sol
    gabi-sol

--- Solution1.c
+++ Solution1.c
@@ -2,7 +2,21 @@
 #include "Utill.h"
 
 
-
+int Q1_doFindMaxSubMatrix(int* mat,int BMrow,int BMcol,int SMrow,int SMcol){
+    int temp,currentSum;
+    currentSum=0;
+    for(int i=0;i<BMrow-SMrow;i++){
+        for(int j=0;j<BMcol-Smcol;j++){
+         temp=sumSmallMat((int*)mat,SMrows, SMcols,BMrows);
+         if(currentSum<temp){
+         currentSum=temp;
+         }
+         mat++;
+       }
+      return currentSum;
+     }
+     
+}
 
 
 
