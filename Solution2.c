5a198dd8049d0753962ade2304741138aa07c2cc
barmoshe <1barmoshe1@gmail.com>
2021-11-14 20:58:00 +0200

    Update Solution2.c

--- Solution2.c
+++ Solution2.c
@@ -6,20 +6,53 @@
 #define ROWS 5
 #define COLS 5
 #define COLOR 3
+int checkIfPointExist(startValues,row,col)
+{
+
+}
+
+void initStartValues(int *startValues)
+{
+	for (int i = 0; i < COLOR; i++)
+	{
+		
+		int row = getRand(0, ROWS - 1);
+		int col = getRand(0, COLS - 1);
+		while(checkIfPointExist(startValues,row,col)==1)
+		{
+			row = getRand(0, ROWS - 1);
+			 col = getRand(0, COLS - 1);
+		}
+		int *pointerRow = startValues + (COLS * i) + 0;
+		int *pointerCol = startValues + (COLS * i) + 1;
+		*pointerRow = row;
+		*pointerCol = col;
+		
+
+	}
+	
+		
+}
+void printStartValues(int *startValues)
+{
+	for (int i =0; i <COLOR; i++)
+	{
+		int *pointerRow = startValues + (COLS * i) + 0;
+		int *pointerCol = startValues + (COLS * i) + 1;
+
+		printf("color number [[%d]] starts :\n\trow --%d--\n\tcol --%d--\n", (i+1), *pointerRow, *pointerCol);
+	}
+}
 void q2()
 {
 	//Solution2.h
 	int mat[ROWS][COLS];
-	int row;
-	int col;
 	printf("Mat portion: rows_%d,cols_%d \n", ROWS, COLS);
 	initMatZero((int *)mat, ROWS, COLS);
 	printf("The color amount : %d \n", COLOR);
 	printf("the  mat:    \n");
 	printMat((int *)mat, ROWS, COLS);
 	int startValues[COLOR][2];
-	for (int i = 0; i < COLOR; i++)
-	{
-		
-	}
+	initStartValues((int *)startValues);
+	printStartValues((int *)startValues);
 }
