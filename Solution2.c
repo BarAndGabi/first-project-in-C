33fbdbf88b704f8e6258dd9470e213dc2bf21b88
barmoshe <1barmoshe1@gmail.com>
2021-11-14 17:27:49 +0200

    Update Solution2.c

--- Solution2.c
+++ Solution2.c
@@ -10,10 +10,16 @@ void q2()
 {
 	//Solution2.h
 	int mat[ROWS][COLS];
+	int row;
+	int col;
 	printf("Mat portion: rows_%d,cols_%d \n", ROWS, COLS);
 	initMatZero((int *)mat, ROWS, COLS);
 	printf("The color amount : %d \n", COLOR);
 	printf("the  mat:    \n");
 	printMat((int *)mat, ROWS, COLS);
-	//int startValues[COLOR][2];
+	int startValues[COLOR][2];
+	for (int i = 0; i < COLOR; i++)
+	{
+		
+	}
 }
