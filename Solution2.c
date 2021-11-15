dd678255e6d63d6fc85b15ad1dac7a31014be173
barmoshe <1barmoshe1@gmail.com>
2021-11-15 12:16:09 +0200

    merge bar branch

--- Solution2.c
+++ Solution2.c
@@ -4,10 +4,35 @@
 #include <stdio.h>
 
 #define ROWS 5
-#define COLS 5
+#define COLS 6
 #define COLOR 3
-int checkIfPointExist(startValues, row, col)
+#define VALUESCOL 2
+
+void colorStartPoints(int *mat, int *startValues)
 {
+	for (int i = 0; i < COLOR; i++)
+	{
+		int row = *(startValues + (VALUESCOL * i) + 0);
+		int col = *(startValues + (VALUESCOL * i) + 1);
+		int *currentColorPlace = mat + (row * COLS) + col;
+		*currentColorPlace = i + 1;
+	}
+	printf("\nthe  mat firts painted:    \n");
+	printMat((int *)mat, ROWS, COLS);
+}
+
+int checkIfPointExist(int *startValues, int row, int col, int i)
+{
+	for (int j = 0; j < i; j++)
+	{
+		int checkRow = *(startValues + j * 2);
+		int checkCol = *(startValues + j * 2 + 1);
+		if (checkRow == row && checkCol == col)
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
 
 void initStartValues(int *startValues)
@@ -17,13 +42,13 @@ void initStartValues(int *startValues)
 
 		int row = getRand(0, ROWS - 1);
 		int col = getRand(0, COLS - 1);
-		while (checkIfPointExist(startValues, row, col) == 1)
+		while (checkIfPointExist((int *)startValues, row, col, i + 1) == 0)
 		{
 			row = getRand(0, ROWS - 1);
 			col = getRand(0, COLS - 1);
 		}
-		int *pointerRow = startValues + (COLS * i) + 0;
-		int *pointerCol = startValues + (COLS * i) + 1;
+		int *pointerRow = startValues + (VALUESCOL * i) + 0;
+		int *pointerCol = startValues + (VALUESCOL * i) + 1;
 		*pointerRow = row;
 		*pointerCol = col;
 	}
@@ -32,15 +57,27 @@ void printStartValues(int *startValues)
 {
 	for (int i = 0; i < COLOR; i++)
 	{
-		int *pointerRow = startValues + (COLS * i) + 0;
-		int *pointerCol = startValues + (COLS * i) + 1;
+
+		int *pointerRow = startValues + (VALUESCOL * i) + 0;
+		int *pointerCol = startValues + (VALUESCOL * i) + 1;
 
 		printf("color number [[%d]] starts :\n\trow --%d--\n\tcol --%d--\n", (i + 1), *pointerRow, *pointerCol);
 	}
 }
+int checkColorAmount()
+{
+	if (COLOR > COLS * ROWS)
+		return 0;
+	return 1;
+}
 void q2()
 {
 	//Solution2.h
+	if (checkColorAmount() == 0)
+	{
+		printf("Too much colors for the mat");
+		return;
+	}
 	int mat[ROWS][COLS];
 	printf("Mat portion: rows_%d,cols_%d \n", ROWS, COLS);
 	initMatZero((int *)mat, ROWS, COLS);
@@ -50,4 +87,7 @@ void q2()
 	int startValues[COLOR][2];
 	initStartValues((int *)startValues);
 	printStartValues((int *)startValues);
+	colorStartPoints((int *)mat, (int *)startValues);
+
+	//colorTheBoard((int*)mat,(int *)startValues);
 }
