8744eb916ca5d0a4259fb331e39839d64586a84f
barmoshe <1barmoshe1@gmail.com>
2021-11-16 09:13:25 +0200

    Update Solution3.c

--- Solution3.c
+++ Solution3.c
@@ -3,9 +3,12 @@
 #include "arrays.h"
 #include <stdio.h>
 
-
+#define ROWS 5
+#define COLS 5
 void q3()
 {
-	//Solution3.h
 	printf("houston we have a problem  the user chose Q3\n");
+	int mat[ROWS][COLS] = {{1, 1, 1, 1, 1}, {1, 0, 1, 0, 1}, {0, 1, 1, 1, 0}, {1, 1, 0, 1, 1}, {1, 1, 1, 1, 1}};
+	int leftNums[ROWS][(COLS + 1) / 2] = {{0, 0, 5}, {1, 1, 1}, {0, 3, 0}, {0, 2, 2}, {0, 0, 5}};
+	
 }
