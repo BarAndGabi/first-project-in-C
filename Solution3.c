486a952e18ae776c19c121ce2833890bac53b17c
barmoshe <1barmoshe1@gmail.com>
2021-11-18 14:23:20 +0200

    3 solved

--- Solution3.c
+++ Solution3.c
@@ -21,16 +21,64 @@ void printAnswer(int answerBool)
 	else
 		printf("\n--------ANSWER IS TRUE--------\n");
 }
+int checkUntilZero(int *row, int currentLenth, int start)
+{
 
+	int switchOn = 0;
+	int currentValue = 0;
+	int counterBlack = 0;
+	if (currentLenth == counterBlack)
+		return start;
+	else
+	{
+		for (int i = start; i < COLS; i++)
+		{
+			currentValue = *(row + i);
+			if (currentValue == 1)
+			{
+				switchOn = 1;
+				counterBlack = counterBlack + 1;
+				if (counterBlack > currentLenth)
+				{
+					return -1;
+				}
+			}
+			else
+			{
+				if (switchOn == 1)
+				{
+					if (counterBlack < currentLenth)
+						return -1;
+					else
+						return i;
+				}
+			}
+		}
+	}
+	return start + 1;
+}
 int checkRow(int *row, int *leftNumsRow)
 {
-	for(int i=0;i<((COLS + 1) / 2);i++)
-{
-}
+	int start = 0;
+	int temp = 0;
+	for (int col = 0; col < ((COLS + 1) / 2); col++)
+	{
+		if (start != -1)
+		{
 
-return 0;
-}
+			int *current = leftNumsRow + col;
+			int currentLenth = *current;
+			temp = checkUntilZero(row, currentLenth, start);
+			start = temp;
+		}
+		else
+		{
+			return 0;
+		}
+	}
 
+	return 1;
+}
 void printRow(int *row, int *leftNumsRow)
 {
 	printf("\nrow is :    ");
@@ -38,15 +86,13 @@ void printRow(int *row, int *leftNumsRow)
 	printf("\nLEFFNUMS is :    ");
 	printArray(leftNumsRow, ((COLS + 1) / 2));
 	printf("\n");
-	
 }
-
 int checkBoard(int *mat, int *leftNums)
 {
 	for (int row = 0; row < ROWS; row++)
 	{
-		printRow(mat + (row * COLS), leftNums + (row * ((COLS + 1) / 2))); checked row send (its good).
-		if (0 == checkRow(mat + (row * COLS), leftNums + (row * COLS)))
+		printRow(mat + (row * COLS), leftNums + (row * ((COLS + 1) / 2))); //checked row send (its good).
+		if (0 == checkRow(mat + (row * COLS), leftNums + (row * ((COLS + 1) / 2))))
 			return 0;
 	}
 	return 1;
@@ -55,7 +101,7 @@ void q3()
 {
 	printf("houston we have a problem  the user chose Q3\n");
 	int mat[ROWS][COLS] = {{1, 1, 1, 1, 1}, {1, 0, 1, 0, 1}, {0, 1, 1, 1, 0}, {1, 1, 0, 1, 1}, {1, 1, 1, 1, 1}};
-	int leftNums[ROWS][(COLS + 1) / 2] = {{0, 0, 5}, {1, 1, 1}, {0, 3, 0}, {0, 2, 2}, {0, 0, 5}};
+	int leftNums[ROWS][(COLS + 1) / 2] = {{0, 1, 5}, {1, 1, 1}, {0, 3, 0}, {0, 2, 2}, {0, 0, 5}};
 	q3SetUp((int *)mat, (int *)leftNums);
 	int answerBool = checkBoard((int *)mat, (int *)leftNums);
 	printAnswer(answerBool);
--- Solution3.h
+++ Solution3.h
@@ -1,6 +1,11 @@
 #ifndef SOLUTION3_H_
 #define SOLUTION3_H_
 void q3();
-
+void printAnswer(int answerBool);
+void q3SetUp(int *mat, int *leftNums);
+int checkUntilZero(int *row, int currentLenth, int start);
+int checkRow(int *row, int *leftNumsRow);
+void printRow(int *row, int *leftNumsRow);
+int checkBoard(int *mat, int *leftNums);
 
 #endif
