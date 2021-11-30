458e751bf596033122152af653b216ee7eea8eb9
barmoshe <1barmoshe1@gmail.com>
2021-11-30 09:17:36 +0200

    Update Solution3.c

--- Solution3.c
+++ Solution3.c
@@ -7,94 +7,118 @@
 #define COLS 5
 #define LEFTCOLS ((COLS + 1) / 2)
 
-int checkRowZero(int *row, int size)
+void q3SetUp(int *mat, int *leftNums)
 {
-	for (int col = 0; col < size; col++)
-	{
-		int currentValue = *(row + col);
-		if (currentValue != 0)
-			return 0;
-	}
-	return 1;
+	printf("The  game board:    \n");
+	printMat(mat, ROWS, COLS);
+	printf("The  left nums:    \n");
+	printMat(leftNums, ROWS, (COLS + 1) / 2);
 }
-int checkZeroCases(int *row, int *colIndex)
+void printAnswer(int answerBool)
 {
+<<<<<<< HEAD
 	if (*colIndex % COLS == 0)
 		while (*row == 0)
 		{
 			row++;
 			*colIndex += 1;
 		}
+=======
+	if (answerBool == 0)
+		printf("\n--------ANSWER IS FALSE--------\n");
+>>>>>>> parent of 308168b (done)
 	else
-	{
-		do
-		{
-			if (*row != 0)
-				return 0;
-			row++;
-			*colIndex += 1;
-		} while (*row == 0);
-	}
-	return 1;
+		printf("\n--------ANSWER IS TRUE--------\n");
 }
-int checkRowColor(int *row, int currentLenth, int *colIndex, int* blackCounter)
+int checkUntilZero(int *row, int currentLenth, int start)
 {
-	if (checkZeroCases((int *)(row + *colIndex), colIndex) == 1)
+	if (currentLenth == 0)
+		return start;
+	int counterBlack = 0;
+	for (int i = start; i < COLS; i++)
 	{
-		if (*colIndex >= COLS)
-			return *colIndex;
-		row += *colIndex;
-		for (int i = 0; i < currentLenth; i++)
+		int currentValue = *(row + i);
+		if (counterBlack == currentLenth)
 		{
-			if (*row == 0)
-				return 0;
-			*blackCounter += 1;
-			row++;
-			*colIndex += 1;
+			printf("\n%d ) ====> \ncurrentValue =====> %d\n", i, currentValue);
+			return i;
 		}
-		return 1;
-	}
-	return 0;
-}
-
-int checkRow(int *row, int *leftNumCol)
-{
-	int lenthSum = 0;
-	int blackCounter = 0;
-	int colIndex = 0;
-	for (int j = 0; j < (COLS + 1) / 2; j++)
-	{
-		lenthSum += *leftNumCol;
-		leftNumCol++;
-	}
-	if (lenthSum > COLS)
-		return 0;
-	else if (lenthSum == 0)
-		return checkRowZero(row, COLS);
-	leftNumCol -= ((COLS + 1) / 2);
-	for (int j = 0; j < LEFTCOLS; j++)
-	{
-		if (*leftNumCol == 0)
-			leftNumCol++;
+		if (currentValue == 0)
+		{
+			if (counterBlack == currentLenth)
+				return i;
+			if (counterBlack > currentLenth)
+				return -1;
+			if (counterBlack < currentLenth && counterBlack > 0)
+				return -1;
+		}
 		else
 		{
-			if (checkRowColor(row, *leftNumCol, &colIndex, &blackCounter) == 0)
-				return 0;
-			leftNumCol++;
+			printf("\n%d ) ====> \ncurrentValue =====> %d\n", currentLenth, currentValue);
+			if (counterBlack < currentLenth && counterBlack >= 0)
+				counterBlack++;
+			if (counterBlack > currentLenth)
+				return -1;
+			if (counterBlack == currentLenth)
+				return i + 1;
 		}
 	}
-	if (colIndex < COLS)
-		if (checkRowColor(row, 0, &colIndex, &blackCounter) == 0)
-			return 0;
+	if (counterBlack == 0)
+		return start;
+	if (counterBlack == currentLenth)
+		return COLS;
+	if (counterBlack < currentLenth || counterBlack > currentLenth)
+		return -1;
 
-	if (blackCounter != lenthSum){
-
-return 0;
-
+	return start;
 }
+int checkRowZero(int *row)
+{
+	for (int col = 0; col < COLS; col++)
+	{
+		int currentValue = *(row + col);
+		if (currentValue != 0)
+			return -1;
+	}
 	return 1;
 }
-
+int checkRow(int *row, int *leftNumsRow)
+{
+	int start = 0;
+	int temp = 0;
+	for (int col = 0; col < ((COLS + 1) / 2); col++)
+	{
+		int *current = leftNumsRow + col;
+		int currentLenth = *current;
+		printf("\n%d ) ====> \nSTART IS =====> %d\n", col + 1, start);
+		temp = checkUntilZero(row, currentLenth, start);
+		if (temp == -1)
+		{
+			return 0;
+		}
+		if (temp == COLS)
+			return 1;
+		start = temp;
+	}
+	if (start != -1)
+	{
+		if (start == 0)
+		{
+			if (checkRowZero(row) > 0)
+				return 1;
+			else
+				return 0;
+		}
+		if (start == temp)
+			return 0;
+		else
+			return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
 void printRow(int *row, int *leftNumsRow)
 {
 	printf("\nrow is :    ");
@@ -107,32 +131,17 @@ int checkBoard(int *mat, int *leftNums)
 {
 	for (int row = 0; row < ROWS; row++)
 	{
-		printRow(mat + (row * COLS), leftNums + (row * LEFTCOLS)); //checked row send (its good).
-		if (0 == checkRow(mat + (row * COLS), leftNums + (row * LEFTCOLS)))
+		printRow(mat + (row * COLS), leftNums + (row * ((COLS + 1) / 2))); //checked row send (its good).
+		if (0 == checkRow(mat + (row * COLS), leftNums + (row * ((COLS + 1) / 2))))
 			return 0;
 	}
 	return 1;
 }
-void q3SetUp(int *mat, int *leftNums)
-{
-	printf("The  game board:    \n");
-	printMat(mat, ROWS, COLS);
-	printf("The  left nums:    \n");
-	printMat(leftNums, ROWS, (COLS + 1) / 2);
-}
-void printAnswer(int answerBool)
-{
-	if (answerBool == 0)
-		printf("\n--------ANSWER IS FALSE--------\n");
-	else
-		printf("\n--------ANSWER IS TRUE--------\n");
-}
-
 void q3()
 {
 	printf("houston we have a problem  the user chose Q3\n");
-	int mat[ROWS][COLS] = {{0, 0, 0, 0, 0}, {1, 0, 1, 0, 1}, {0, 1, 1, 1, 0}, {1, 1, 0, 1, 1}, {1, 1, 1, 1, 1}};
-	int leftNums[ROWS][(COLS + 1) / 2] = {{0, 0, 0}, {1, 1, 1}, {3, 0, 0}, {0, 2, 2}, {3, 0, 1}};
+	int mat[ROWS][COLS] = {{0, 0, 0, 0, 1}, {0, 0, 0, 0, 1}, {0, 1, 1, 1, 0}, {1, 1, 0, 1, 1}, {1, 1, 1, 1, 1}};
+	int leftNums[ROWS][(COLS + 1) / 2] = {{0, 0, 1}, {0, 0, 1}, {3, 0, 1}, {0, 2, 2}, {0, 0, 5}};
 	q3SetUp((int *)mat, (int *)leftNums);
 	int answerBool = checkBoard((int *)mat, (int *)leftNums);
 	printAnswer(answerBool);
